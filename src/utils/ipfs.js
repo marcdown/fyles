3cc756605a406f357d2092919f1911a448e403af
Marc Brown <heymarcbrown@gmail.com>
2018-08-27 01:12:49 -0400

    Default to Infura IPFS node

--- src/utils/ipfs.js
+++ src/utils/ipfs.js
@@ -1,9 +1,9 @@
 const ipfsApi = require('ipfs-api');
 
 // Local daemon
-const ipfs = new ipfsApi({ host: 'localhost', port: 5001, protocol: 'http' });
+//const ipfs = new ipfsApi({ host: 'localhost', port: 5001, protocol: 'http' });
 
 // Infura daemon
-//const ipfs = new ipfsApi({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+const ipfs = new ipfsApi({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
 export default ipfs
